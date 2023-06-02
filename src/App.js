aa3f969a7267466d2220f51493650ed748eb1cdb
stevens4908 <saturno4908@gmail.com>
2023-06-02 17:40:29 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Login from './components/login/Login';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/login/Login.js
@@ -0,0 +1,79 @@
+import React, {useState} from "react";
+import './Login.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const Login = () => {
+
+    const [Email , setEmail] = useState('')
+    const [Password , setPassword] = useState('')
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if(Email === "sha" & Password === "1234" ) {
+            window.open("https://youtube.com/")
+        }
+        else {
+            alert("credenciales incorrectas")
+        }
+
+        
+    }
+
+return(
+
+<React.Fragment>
+
+
+    <Container className="">
+      <Row>
+        <Col>
+
+            <Form onSubmit={handleSubmit}>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label >Email</Form.Label>
+                    <Form.Control type="text" placeholder="Enter email" value={Email} onChange={handleEmailChange}/>
+                    
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Contraseña</Form.Label>
+                    <Form.Control type="password" placeholder="Password" value={Password} onChange={handlePasswordChange}/>
+                </Form.Group>
+
+                
+                <Button variant="primary" type="submit">
+                    Login
+                </Button>
+
+            </Form>
+
+        </Col>
+      </Row>
+    </Container>
+        
+
+
+</React.Fragment>
+
+
+)
+
+
+}
+
+export default Login;
