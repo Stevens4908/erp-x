62b6d6f6ffa6b2fc1a9bd83944e781d406e2fd2d
eztibenz <saturno4908@gmail.com>
2023-06-04 09:36:23 -0500

    version3

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from '../pages/Home'
+import Members from '../pages/Members'
+
+const Dashboard = () => {
+
+    return(
+        <div>
+
+        </div>
+    )
+
+}
+
+export default Dashboard;
---
+++ src/components/pages/Members.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Members = () => {
+
+return(
+
+<div>
+hello Members
+</div>
+
+)
+
+}
+
+export default Members
